5e59bc7349c1db5e44fce831579e88bc844a5616
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-07-07 13:28:15 +0545

    CRUD operations

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'This returns a list of all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This return a ${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This update ${id} coffee with data: ${JSON.stringify(body)}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This delete ${id} coffee`;
   }
 }
