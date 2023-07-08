87a9d9dd463f145231a809d66bd4b3bcf3160b87
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-07-08 23:54:36 +0545

    Docker with Postgres and TypeORM setup

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,17 @@
 /* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
