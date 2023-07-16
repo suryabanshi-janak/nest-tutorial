fd86748c393089e00a6f6cf4a84afa1a51f5acf5
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2023-07-16 22:45:20 +0545

    Add pointcuts with interceptors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 // import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 
 async function bootstrap() {
@@ -28,6 +29,7 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   // app.useGlobalGuards(new ApiKeyGuard());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
